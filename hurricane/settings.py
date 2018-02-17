7ffbd4aab2c255c6dcfc9a2e8c7e3b6d6f986cf8
Ilya Cherepanov <cherepanov.ilya@gmail.com>
2018-02-17 21:04:25 +0700

    Added hurricanelog.herokuapp.com to ALLOWED_HOSTS

--- hurricane/settings.py
+++ hurricane/settings.py
@@ -24,9 +24,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e7ac043gv2)slyc+8ykx#-u$c(8$gv!8w1cx=b%_twv#lyl)j='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'hurricanelog.herokuapp.com',
+]
 
 
 # Application definition
