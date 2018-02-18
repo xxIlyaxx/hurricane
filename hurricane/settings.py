0de2ec6e3abcbec1d0cd77e502b65897fa0922ad
Ilya Cherepanov <cherepanov.ilya@gmail.com>
2018-02-18 14:04:50 +0700

    Fixed work with static files

--- hurricane/settings.py
+++ hurricane/settings.py
@@ -28,6 +28,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = [
     'hurricanelog.herokuapp.com',
+    'localhost',
 ]
 
 
@@ -126,7 +127,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "hurricane", "static")
