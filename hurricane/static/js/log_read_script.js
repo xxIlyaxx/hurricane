93f1f87666c2fdcf4a8be419448a244567b97cb9
Ilya Cherepanov <cherepanov.ilya@gmail.com>
2018-02-18 15:45:12 +0700

    Added scroll by up and down arrow keys

--- hurricane/static/js/log_read_script.js
+++ hurricane/static/js/log_read_script.js
@@ -1,12 +1,26 @@
 $(document).ready(function() {
     var logcontent = $("#logcontent");
     var lineHeight = parseFloat(logcontent.css("line-height"));
+    var SKIP_LINES = 5;
 
     $(".btn").on("click", function() {
         if ($(this).hasClass("btn-up")) {
-            logcontent.scrollTop(logcontent.scrollTop() - lineHeight);
+            logcontent.scrollTop(logcontent.scrollTop() - lineHeight * SKIP_LINES);
         } else {
-            logcontent.scrollTop(logcontent.scrollTop() + lineHeight);
+            logcontent.scrollTop(logcontent.scrollTop() + lineHeight * SKIP_LINES);
+        }
+    });
+
+    $(document).on("keydown", function(event) {
+        switch(event.which) {
+            case 38:
+                logcontent.scrollTop(logcontent.scrollTop() - lineHeight * SKIP_LINES);
+                break;
+            case 40:
+                logcontent.scrollTop(logcontent.scrollTop() + lineHeight * SKIP_LINES);
+                break;
+            default:
+                return;
         }
     });
 
